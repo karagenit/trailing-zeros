6b46f3c4e9d862430a59d46859c6fba03e5d0192
Caleb Smith <karagenit@outlook.com>
2018-10-01 14:52:54 -0400

    Add Script

---
+++ zeros.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+print "Enter factorial value: "
+input = gets.chomp.to_i
+zeros = 0
+
+for i in (1..input)
+  val = i
+  while (val % 5) == 0
+    zeros += 1
+    val /= 5
+  end
+end
+
+puts "Trailing zeros in #{input}! is #{zeros}."
